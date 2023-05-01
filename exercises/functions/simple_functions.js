044e5be7656cdc0ca0a7e6e0800c863a57f17522
MyoMin <MayoMinPhai@gmail.com>
2023-05-01 11:35:42 -0400

    initial commit

---
+++ exercises/functions/simple_functions.js
@@ -0,0 +1,14 @@
+function favoriteThings(){
+ console.log("Myo's favorite things are: ");
+console.log("My favorite movie is 21 Jump Street");
+console.log("My favorite band is One OK Rock");
+
+}
+favoriteThings();
+
+function whyImHere() {
+    console.log(
+        '    I enjoy the program and knowledge gain and the cohort.'   
+    );
+}
+whyImHere()
