0312b277142d7262933d021b9c2fd0ae35894670
MyoMin <MayoMinPhai@gmail.com>
2023-05-01 15:15:57 -0400

    exercises com

--- exercises/functions/more_functions.js
+++ exercises/functions/more_functions.js
@@ -15,10 +15,19 @@ let homeState = "PA";
 let homeZip = "15225";
 displayMailingLabel(fullName, homeAddress, homeCity, homeState, homeZip);
 
-function addNumbers(num1, num2){
-let result = num1 + num2;
-console.log(result);
-let output = `${num1} + ${num2} = ${result}`;
-
+function addNumbers(num1, num2) {
+  let result = num1 + num2;
+  console.log(result);
+  let output = `${num1} + ${num2} = ${result}`;
 }
-addNumbers(4, 2)
+addNumbers(4, 2);
+
+function displayReceipt(amountPaid, totalDue) {
+  let changeDue = amountPaid - totalDue;
+  let output = `${amountPaid} - ${totalDue} = ${changeDue}
+
+`;
+
+  console.log("$" + changeDue);
+}
+displayReceipt(60, 30);
