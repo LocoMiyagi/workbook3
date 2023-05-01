90484932342b7139eb76121db1cb244200d6dee6
MyoMin <MayoMinPhai@gmail.com>
2023-05-01 15:35:00 -0400

    xtra

--- exercises/functions/c_to_f.js
+++ exercises/functions/c_to_f.js
@@ -1,8 +1,8 @@
 function convertCtoF(celsiusTemperature){
-let fahrenheitTemperature = (celsiusTemperature * (9/5) + 32);
+const fahrenheitTemperature = (celsiusTemperature * (9/5) + 32);
 return fahrenheitTemperature;
 
 }
-let currentTemp = 7;
-let fahrenheitTemperature = convertCtoF(currentTemp);
-console.log(fahrenheitTemperature);
+const currentTemp = 7;
+const fahrenheitTemperature = convertCtoF(currentTemp);
+console.log(fahrenheitTemperature.toFixed(2));
